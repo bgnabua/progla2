ae34adc0a2d3295c36eb57b2b70cfc4098a720f7
Arnold Benedict G. Nabua <nabuabenedict@yahoo.com>
2016-12-10 17:55:10 +0800

    Create mini_calc.cpp

---
+++ mini_calc.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	int a, b, res;
+	cout<<"Enter two number :";
+	cin>>a>>b;
+	res=a+b;
+	cout<<"\nSum = "<<res;
+	res=a-b;
+	cout<<"\nDifference = "<<res;
+	res=a*b;
+	cout<<"\nProduct = "<<res;
+	res=a/b;
+	cout<<"\nQuotient = "<<res;
+return 0;
+}
