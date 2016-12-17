8f7c161fe91d888df81af3bcda1283c948fed709
Arnold Benedict G. Nabua <nabuabenedict@yahoo.com>
2016-12-17 16:46:34 +0800

    Create structure.cpp

---
+++ structure.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+
+struct Person
+{
+
+	int toes ;
+	int knees ;
+	int shoulder ; 
+	int head ;
+	
+	
+
+};
+
+int main()
+
+{
+	Person Person;
+  cin>>Person.toes;
+  cin>>Person.knees;
+  cin>>Person.shoulder;
+  cin>>Person.head;
+  
+  cout<<Person.toes<<"\n";
+  cout<<Person.knees<<"\n";
+  cout<<Person.shoulder<<"\n";
+  cout<<Person.head<<"\n";
+	
+	
+	
+	return 0;
+}
+	
+	
+	
+	
+	
